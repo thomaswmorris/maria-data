e144457901e286014c8d11a769baea4895f4b721
Thomas Morris <thomasmorris@princeton.edu>
2025-09-28 11:40:26 -0400

    update spectra to v3

--- scripts/compute_atmospheric_spectra.sh
+++ scripts/compute_atmospheric_spectra.sh
@@ -1,4 +1,4 @@
-for region in boolardy boston chajnantor chiang_mai effelsberg green_bank mauna_kea meerkat metsahovi minamimaki mount_graham narrabri ngari owens_valley pic_de_bure pico_veleta princeton qitai san_agustin san_basilio sierra_negra south_pole summit_camp teide thule; do
+for region in boolardy boston chajnantor chiang_mai effelsberg green_bank mauna_kea meerkat metsahovi minamimaki mount_graham narrabri new_haven ngari owens_valley pic_de_bure pico_veleta princeton qitai san_agustin san_basilio sierra_negra south_pole summit_camp teide thule; do
     python /users/tom/maria/data/scripts/compute_atmospheric_spectrum.py --region $region --tag v2
 done
 
